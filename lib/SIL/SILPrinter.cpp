f939793625194beb830e749dac5dc328e5926a83
Arnold Schwaighofer <aschwaighofer@apple.com>
2017-01-27 15:35:33 -0800

    SIL: Add an allowed access kind to the opened value of an open_existential_addr instruction
    Once we move to a copy-on-write implementation of existential value buffers we
    can no longer consume or destroy values of an opened existential unless the
    buffer is uniquely owned.
    
    Therefore we need to track the allowed operation on opened values.
    
    Add qualifiers "mutable_access" and "immutable_access" to open_existential_addr
    instructions to indicate the allowed access to the opened value.
    
    Once we move to a copy-on-write implementation, an "open_existential_addr
    mutable_access" instruction will ensure unique ownership of the value buffer.

--- lib/SIL/SILPrinter.cpp
+++ lib/SIL/SILPrinter.cpp
@@ -1478,6 +1478,10 @@ public:
     *this << DMI->getType();
   }
   void visitOpenExistentialAddrInst(OpenExistentialAddrInst *OI) {
+    if (OI->getAccessKind() == OpenedExistentialAccess::Immutable)
+      *this << "immutable_access ";
+    else
+      *this << "mutable_access ";
     *this << getIDAndType(OI->getOperand()) << " to " << OI->getType();
   }
   void visitOpenExistentialRefInst(OpenExistentialRefInst *OI) {
