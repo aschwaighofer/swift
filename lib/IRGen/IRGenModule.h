059028042eb08f28b6279aaa7acd667283816bdb
Arnold Schwaighofer <aschwaighofer@apple.com>
2017-06-12 11:24:53 -0700

    Use clang's effective llvm triple for IR generation
    Instead of using the target that was passed to the driver. Use the target from
    the clang importer that might have been changed by clang (i.e armv7 to thumbv7
    on darwin)
    
    rdar://32599805

--- lib/IRGen/IRGenModule.h
+++ lib/IRGen/IRGenModule.h
@@ -353,6 +353,9 @@ public:
     }
     return nullptr;
   }
+
+  /// Return the effective triple used by clang.
+  llvm::Triple getEffectiveClangTriple();
 };
 
 class ConstantReference {
@@ -391,7 +394,7 @@ public:
   llvm::Module &Module;
   llvm::LLVMContext &LLVMContext;
   const llvm::DataLayout DataLayout;
-  const llvm::Triple &Triple;
+  const llvm::Triple Triple;
   std::unique_ptr<llvm::TargetMachine> TargetMachine;
   ModuleDecl *getSwiftModule() const;
   Lowering::TypeConverter &getSILTypes() const;
