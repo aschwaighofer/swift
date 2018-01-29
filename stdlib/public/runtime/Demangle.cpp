eb63e7812e0af88eeb4323e6a5816dfb301ce1af
Arnold Schwaighofer <aschwaighofer@apple.com>
2018-01-29 15:05:24 -0800

    Add EscapingFunctionType to _swift_buildDemanglingForMetadata

--- stdlib/public/runtime/Demangle.cpp
+++ stdlib/public/runtime/Demangle.cpp
@@ -461,7 +461,10 @@ swift::_swift_buildDemanglingForMetadata(const Metadata *type,
     Node::Kind kind;
     switch (func->getConvention()) {
     case FunctionMetadataConvention::Swift:
-      kind = Node::Kind::FunctionType;
+      if (!func->isEscaping())
+        kind = Node::Kind::FunctionType;
+      else
+        kind = Node::Kind::EscapingFunctionType;
       break;
     case FunctionMetadataConvention::Block:
       kind = Node::Kind::ObjCBlock;
