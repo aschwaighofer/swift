954fa871fd69b50b1332f8268d7665c373b5884c
Nate Chandler <nate_chandler@apple.com>
2020-11-09 17:35:04 -0800

    [TBDGen] Add async function pointers.
    In order to call async functions, instances of the AsyncFunctionPointer
    struct must be used.  If those functions are exported from a module, the
    AsyncFunctionPointer by means of which the function is to be called must
    be exported as well.
    
    For now, the symbol is exported by manually appending the relevant
    suffix to the mangled name of the function.

--- lib/TBDGen/TBDGen.cpp
+++ lib/TBDGen/TBDGen.cpp
@@ -718,6 +718,10 @@ void TBDGenVisitor::visitAbstractFunctionDecl(AbstractFunctionDecl *AFD) {
                        AFD->getGenericSignature()));
 
   visitDefaultArguments(AFD, AFD->getParameters());
+
+  if (AFD->isAsyncContext()) {
+    addSymbol(LinkEntity::forAsyncFunctionPointer(AFD));
+  }
 }
 
 void TBDGenVisitor::visitFuncDecl(FuncDecl *FD) {
