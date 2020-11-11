1410e930d35f51bdd8abf91fbb2f962b3e5a6d42
Arnold Schwaighofer <aschwaighofer@apple.com>
2020-11-11 09:30:56 -0800

    Add to async.swift irgen test

--- test/IRGen/async.swift
+++ test/IRGen/async.swift
@@ -8,3 +8,45 @@ public func f() async { }
 // CHECK: "$s5async1gyyYKF"
 public func g() async throws { }
 
+public func hello(_ p: Int) async {
+  print("hello")
+}
+
+public func callHello() async {
+  await hello(5)
+}
+
+public func returnSomething(_ p: Int) async -> Int {
+  return p
+}
+
+public func callReturnSomething() async {
+  let x = await returnSomething(5)
+  print(x)
+  let y = await returnSomething(6)
+  print(y)
+}
+
+public func callAsyncClosure(_ closure : () async->()) async {
+  await closure()
+}
+
+public class SomeClass {}
+
+internal func aPrivateAsync(_ x: Int) async -> Int {
+  return x
+}
+
+public func useAsyncClosure(_ c: SomeClass) async {
+  func closure() async {
+    print(c)
+  }
+  await callAsyncClosure(closure)
+}
+
+public func createAsyncClosure<T>(_ t: T) async -> () async -> () {
+  func closure() async {
+    print(t)
+  }
+  return closure
+}
