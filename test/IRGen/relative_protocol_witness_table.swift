6fc773870c74111ccf41614a7df11c5300f7d96a
Arnold Schwaighofer <aschwaighofer@apple.com>
2023-01-10 07:21:01 -0800

    Test case

--- test/IRGen/relative_protocol_witness_table.swift
+++ test/IRGen/relative_protocol_witness_table.swift
@@ -77,6 +77,19 @@ struct GStruct<T> : WithAssocConformance where T: FuncOnly, T: InitP {
     }
 }
 
+struct ConditionalStruct<T> {
+    init(_ t: T) {}
+}
+
+extension ConditionalStruct : WithAssocConformance where T: FuncOnly, T: InitP {
+  func initAssoc() -> T {
+      return T()
+  }
+}
+
+func instantiate_conditional_conformance<T>(_ t : T) where T: FuncOnly, T: InitP {
+  requireWitness4(ConditionalStruct(t))
+}
 // Relative protocol witness table.
 
 // Simple Table.
@@ -135,6 +148,19 @@ struct GStruct<T> : WithAssocConformance where T: FuncOnly, T: InitP {
 // CHECK-SAME:                      i64 ptrtoint (i32* getelementptr inbounds ([4 x i32], [4 x i32]* @"$s1A7GStructVyxGAA20WithAssocConformanceAAWP", i32 0, i32 3) to i64)) to i32)
 // CHECK-SAME: ], align 8
 
+// Conditional conformance
+
+// CHECK: @"$s1A17ConditionalStructVyxGAA20WithAssocConformanceA2A8FuncOnlyRzAA5InitPRzlWP" = hidden constant [4 x i32]
+// CHECK-SAME: [i32 trunc (i64 sub (i64 ptrtoint ({ i32, i32, i32, i32, i32, i32, i32, i32, i32, i32, i16, i16, i32, i32 }* @"$s1A17ConditionalStructVyxGAA20WithAssocConformanceA2A8FuncOnlyRzAA5InitPRzlMc" to i64),
+// CHECK-SAME:                      i64 ptrtoint ([4 x i32]* @"$s1A17ConditionalStructVyxGAA20WithAssocConformanceA2A8FuncOnlyRzAA5InitPRzlWP" to i64)) to i32),
+// CHECK-SAME:  i32 trunc (i64 sub (i64 ptrtoint (i8* getelementptr (i8, i8* getelementptr inbounds (<{ i8, i8, i32, i8 }>, <{ i8, i8, i32, i8 }>* @"associated conformance 1A17ConditionalStructVyxGAA20WithAssocConformanceA2A8FuncOnlyRzAA5InitPRzl0D4TypeAaEP_AaF", i32 0, i32 0), i64 1) to i64),
+// CHECK-SAME:                      i64 ptrtoint (i32* getelementptr inbounds ([4 x i32], [4 x i32]* @"$s1A17ConditionalStructVyxGAA20WithAssocConformanceA2A8FuncOnlyRzAA5InitPRzlWP", i32 0, i32 1) to i64)) to i32),
+// CHECK-SAME:  i32 trunc (i64 sub (i64 ptrtoint (i8* getelementptr inbounds (<{ [1 x i8], i8 }>, <{ [1 x i8], i8 }>* @"symbolic x", i32 0, i32 0, i64 1) to i64),
+// CHECK-SAME:                      i64 ptrtoint (i32* getelementptr inbounds ([4 x i32], [4 x i32]* @"$s1A17ConditionalStructVyxGAA20WithAssocConformanceA2A8FuncOnlyRzAA5InitPRzlWP", i32 0, i32 2) to i64)) to i32),
+// CHECK-SAME:  i32 trunc (i64 sub (i64 ptrtoint (void (%swift.opaque*, %T1A17ConditionalStructV*, %swift.type*, i8**)* @"$s1A17ConditionalStructVyxGAA20WithAssocConformanceA2A8FuncOnlyRzAA5InitPRzlAaEP04initD00D4TypeQzyFTW" to i64),
+// CHECK-SAME:                      i64 ptrtoint (i32* getelementptr inbounds ([4 x i32], [4 x i32]* @"$s1A17ConditionalStructVyxGAA20WithAssocConformanceA2A8FuncOnlyRzAA5InitPRzlWP", i32 0, i32 3) to i64)) to i32)
+// CHECK-SAME: ], align 8
+
 // Simple witness entry access.
 
 // CHECK: define{{.*}} swiftcc void @"$s1A14requireWitnessyyxAA8FuncOnlyRzlF"(%swift.opaque* noalias nocapture {{%.*}}, %swift.type* {{%.*}}, i8** [[PWT:%.*]])
@@ -178,10 +204,35 @@ struct GStruct<T> : WithAssocConformance where T: FuncOnly, T: InitP {
 // Accessing an associated witness
 // TODO: we will probably end up calling a different entry point
 // CHECK: define{{.*}} swiftcc void @"$s1A15requireWitness3yyxAA9WithAssocRzlF"(
-// CHECK:   call{{.*}} swiftcc %swift.metadata_response @swift_getAssociatedTypeWitness(
+// CHECK:   call{{.*}} swiftcc %swift.metadata_response @swift_getAssociatedTypeWitnessRelative(
 // CHECK:   ret void
 
 // CHECK: define{{.*}} swiftcc void @"$s1A15requireWitness4yyxAA20WithAssocConformanceRzlF"(%swift.opaque* {{.*}}, %swift.type* [[TYPE:%.*]], i8** [[PWT:%.*]])
-// CHECK:  [[RES:%.*]] = call{{.*}} swiftcc %swift.metadata_response @swift_getAssociatedTypeWitness(i64 0, i8** [[PWT]], %swift.type* [[TYPE]]
+// CHECK:  [[RES:%.*]] = call{{.*}} swiftcc %swift.metadata_response @swift_getAssociatedTypeWitnessRelative(i64 0, i8** [[PWT]], %swift.type* [[TYPE]]
 // CHECK:  [[ASSOCTYPE:%.*]] = extractvalue %swift.metadata_response [[RES]], 0
-// CHECK:  call{{.*}} swiftcc i8** @swift_getAssociatedConformanceWitness(i8** [[PWT]], %swift.type* [[TYPE]], %swift.type* [[ASSOCTYPE]]
+// CHECK:  call{{.*}} swiftcc i8** @swift_getAssociatedConformanceWitnessRelative(i8** [[PWT]], %swift.type* [[TYPE]], %swift.type* [[ASSOCTYPE]]
+
+// Conditional conformance witness table entry.
+
+// CHECK: define{{.*}} swiftcc void @"$s1A17ConditionalStructVyxGAA20WithAssocConformanceA2A8FuncOnlyRzAA5InitPRzlAaEP04initD00D4TypeQzyFTW"(%swift.opaque* {{.*}}, %T1A17ConditionalStructV* {{.*}}, %swift.type* [[SELF:%.*]], i8** [[SELFWITNESSTBL:%.*]])
+// CHECK: entry:
+// CHECK:   [[T1:%.*]] = bitcast i8** [[SELFWITNESSTBL]] to i32*
+// CHECK:   [[T2:%.*]] = getelementptr inbounds i32, i32* [[T1]], i32 -1
+// CHECK:   [[T3:%.*]] = load i32, i32* [[T2]]
+// CHECK:   [[T4:%.*]] = sext i32 [[T3]] to i64
+// CHECK:   [[T5:%.*]] = ptrtoint i32* [[T2]] to i64
+// CHECK:   [[T6:%.*]] = add i64 [[T5]], [[T4]]
+// CHECK:   [[T7:%.*]] = inttoptr i64 [[T6]] to i8*
+// CHECK:   [[FUNCONLY:%.*]] = bitcast i8* [[T7]] to i8**
+// CHECK:   [[T8:%.*]] = bitcast i8** [[SELFWITNESSTBL]] to i32*
+// CHECK:   [[T9:%.*]] = getelementptr inbounds i32, i32* [[T8]], i32 -2
+// CHECK:   [[T10:%.*]] = load i32, i32* [[T9]]
+// CHECK:   [[T11:%.*]] = sext i32 [[T10]] to i64
+// CHECK:   [[T12:%.*]] = ptrtoint i32* [[T9]] to i64
+// CHECK:   [[T13:%.*]] = add i64 [[T12]], [[T11]]
+// CHECK:   [[T14:%.*]] = inttoptr i64 [[T13]] to i8*
+// CHECK:   [[INITP:%.*]] = bitcast i8* [[T14]] to i8**
+// CHECK:   [[T15:%.*]] = bitcast %swift.type* [[SELF]] to %swift.type**
+// CHECK:   [[T16:%.*]] = getelementptr inbounds %swift.type*, %swift.type** [[T15]], i64 2
+// CHECK:   [[T:%.*]] = load %swift.type*, %swift.type** [[T16]]
+// CHECK:   call{{.*}} swiftcc void @"$s1A17ConditionalStructVA2A8FuncOnlyRzAA5InitPRzlE9initAssocxyF"(%swift.opaque* {{.*}}, %swift.type* [[T]], i8** [[FUNCONLY]], i8** [[INITP]])
