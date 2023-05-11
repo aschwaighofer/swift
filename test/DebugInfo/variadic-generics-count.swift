de98b68a1954619560367f4b52762418086a0b2f
Arnold Schwaighofer <aschwaighofer@apple.com>
2023-05-11 08:33:05 -0700

    Move tests over to use the `%use_no_opaque_pointers` option

--- test/DebugInfo/variadic-generics-count.swift
+++ test/DebugInfo/variadic-generics-count.swift
@@ -1,6 +1,8 @@
 // REQUIRES: asserts
-// RUN: %target-swift-frontend -emit-ir %s -g -o - \
+// RUN: %target-swift-frontend %use_no_opaque_pointers -emit-ir %s -g -o - \
 // RUN:    -parse-as-library -module-name a -enable-experimental-feature VariadicGenerics | %FileCheck %s
+// RUN: %target-swift-frontend -emit-ir %s -g -o - \
+// RUN:    -parse-as-library -module-name a -enable-experimental-feature VariadicGenerics
 
 public func f1<each T>(ts: repeat each T) {
   // CHECK: define {{.*}} @"$s1a2f12tsyxxQp_tRvzlF"(%swift.opaque** {{.*}}, i{{32|64}} [[COUNT1_1:.*]], %swift.type** {{.*}})
