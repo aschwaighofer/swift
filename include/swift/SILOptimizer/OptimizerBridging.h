9feee3514f79027ea5680a792c0e9eca8413354a
Arnold Schwaighofer <aschwaighofer@apple.com>
2023-11-03 09:46:56 -0700

    Add shouldExpand() guard to RedundantLoadElimination

--- include/swift/SILOptimizer/OptimizerBridging.h
+++ include/swift/SILOptimizer/OptimizerBridging.h
@@ -243,6 +243,8 @@ struct BridgedPassContext {
   BRIDGED_INLINE bool optimizeMemoryAccesses(BridgedFunction f) const;
   BRIDGED_INLINE bool eliminateDeadAllocations(BridgedFunction f) const;
 
+  BRIDGED_INLINE bool shouldExpand(BridgedType type) const;
+
   // IRGen
 
   SwiftInt getStaticSize(BridgedType type) const;
